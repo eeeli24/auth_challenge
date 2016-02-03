308128013737fdee22848471f0c3141faaecef13
Ilia Khudiashov <ilia.khudiashov@gmail.com>
2016-02-03 13:12:39 +0300

    clean up and seed db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_url(@user), notice: 'User created'
-    else
-      render :new
-    end
-  end
-
   def edit
   end
 
